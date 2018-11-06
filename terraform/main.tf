98c1d2b1d17e1517d0dbb0dd1d33fc367ff67713
trinathtiru <ctrinath7@gmail.com>
2018-11-06 18:45:10 +0530

    updated main

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,10 @@ provider "aws" {
   
 }
 
-
 resource "aws_instance" "web" {
-  
+    ami = "ami-0f65671a86f061fcd"
+    instance_type = "t2.micro"
+    key_name = "jobs2mon"
+    security_groups = ["ALL TCP N PING"]
+ 
 }
-
-output "outputofweb" {
-  value = "value"
-}
-
